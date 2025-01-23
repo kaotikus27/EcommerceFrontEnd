8d423bb606970dbb2eb6f0274e2d12b821f88f32
ELEOMAR <kaotikus27@gmail.com>
2025-01-23 11:53:44 +0800

    login bug done

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default  {
     oidc:{
         clientId: '0oamtuzsihWwKPoVj5d7',
         issuer: 'https://dev-70903254.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
     },
     authParams: {
